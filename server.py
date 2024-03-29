ab04c718a89e7aa653ebc7a1a896e0984807e8ac
abdeldroid2007 <161964484+abdeldroid2007@users.noreply.github.com>
2024-03-29 00:11:40 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,15 +3,12 @@ import random
 import string
 from flask import Flask, render_template, request, jsonify
 import json
-import sqlite3
+import boto3
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-
-c.execute('''CREATE TABLE IF NOT EXISTS chat_data
-             (id TEXT, uuid TEXT, web_mode INTEGER, result TEXT)''')
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-1', aws_access_key_id='your_access_keyASIA4N2NY7ZRD5EZA6FA', aws_secret_access_key='Vf95aLRU4wWSJ888vGuYthDCMRBMPIDTjH4TDUEL')
+table = dynamodb.Table('fantastic-bear-veilCyclicDB')
 
 def get_uuid():
     response = requests.get("https://www.uuidgenerator.net/api/version4")
@@ -32,10 +29,10 @@ def gen_id():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        txt = request.form.get('text')
-        mode = request.form.get('mode')
-        uuid_1 = request.form.get('uuid')
-        id_1 = request.form.get('id')
+        txt = request.form['text']
+        mode = request.form['mode']
+        uuid_1 = request.form['uuid']
+        id_1 = request.form['id']
         id = gen_id() if id_1 == None else id_1
         uuid = get_uuid() if uuid_1 == None else uuid_1
         webk = True if mode == '1' else False
@@ -50,7 +47,7 @@ def index():
                     'role': 'user'
                 }
             ],
-            'id': id,
+            'id': '6clrFCv',
             'previewToken': None,
             'userId': uuid,
             'codeModelMode': True,
@@ -72,9 +69,15 @@ def index():
         if response.status_code == 200:
             result = response.text
             
-            # Store data in SQLite database
-            c.execute("INSERT INTO chat_data (id, uuid, web_mode, result) VALUES (?, ?, ?, ?)", (id, uuid, int(webk), result))
-            conn.commit()
+            # Store data in DynamoDB table
+            response_dynamodb = table.put_item(
+                Item={
+                    'id': id,
+                    'uuid': uuid,
+                    'web_mode': int(webk),
+                    'result': result
+                }
+            )
 
             return jsonify({
                 'id': id,
